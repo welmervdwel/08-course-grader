f46d4d3d3c0575cc91c3c1266426b9fe1419df28
Abel Goodwin <abelgoodwin1988@gmail.com>
2019-04-12 18:27:27 -0600

    impl tests, readme, and minimal

---
+++ README.md
@@ -0,0 +1,10 @@
+# assignment_name
+
+Write a course_grader function that takes a **list** of test scores as its *parameter*.
+
+It will add up these test scores and calculate an average score.
+
+It should then **return** a message of "pass" or "fail" depending on these two conditions:
+
+>1. If the average score is greater than or equal to 70 and no single test score is below 50, then return a message of "pass".
+>2. If the average score is lower than 70 or at least one test score is below 50, then return a message of "fail".
---
+++ test.py
@@ -0,0 +1,38 @@
+from course_grader import course_grader
+
+test_cases = [
+    {
+        "input": [100, 75, 45],
+        "output":  "fail"
+    },
+    {
+        "input": [100, 70, 85],
+        "output":  "pass"
+    },
+    {
+        "input": [80, 60, 60],
+        "output":  "fail"
+    },
+    {
+        "input": [80, 80, 90, 30, 80],
+        "output":  "fail"
+    },
+    {
+        "input": [70, 70, 70, 70, 70],
+        "output":  "pass"
+    },
+    {
+        "input": [60, 80, 80, 70, 70],
+        "output":  "pass"
+    },
+]
+
+def test_leap_year_test_cases():
+    for test_case in test_cases:
+        assignment_response = assignment(test_case['input'])
+        assert is_leap_year_response == test_case[
+            'output'], f"""
+For
+\nInput:    {test_case['input']}
+\nExpected: {test_case['output']}
+\nGot:      {assignment_response}\n"""
